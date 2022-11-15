8ce36f67134688c411e01e994fd9a84384a9d313
giba <260553@studenti.unimore.it>
2022-11-15 19:41:45 +0100

    added params

---
+++ mqtt/conf/bridge_info.py
@@ -0,0 +1,7 @@
+class BridgeInfo:
+    port = 2323
+    address = "localhost"
+    city = "Castelfranco"
+    manufacturer = "CLG Srl"
+    software_version = "0.1"
+    
---
+++ mqtt/conf/broker_params.py
@@ -0,0 +1,8 @@
+class CloudBrokerParams:
+    BROKER_ADDRESS = "smarterplayer.carminezacc.com"
+    BROKER_PORT = 1883
+    #BROKER_USERNAME = "smarterplayer"
+    #BROKER_PASSWORD = "pswd"
+    BASE_TOPIC = "smarterplayer/"
+    BASKET_TOPIC = "basket/"
+    COURT_TOPIC = "court/"
