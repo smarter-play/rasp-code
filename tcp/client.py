00fc9343fa2e9dc47e64d8a347fb6313e18eed4f
giba <260553@studenti.unimore.it>
2022-12-02 17:35:15 +0100

    for testing purposes

--- tcp/client.py
+++ tcp/client.py
@@ -1,19 +1,21 @@
 import asyncio
 import logging
-import secrets
-from mqtt.conf.packet_structures import TCPPacketStructures as packetStructures
+import os
+import random
 
 SCORE_CODE = b'\x00'
 ACCELEROMETER_CODE = b'\x01'
+CUSTOM_COMMAND_CODE = b'\x02'
 
+CODES = [SCORE_CODE, ACCELEROMETER_CODE, CUSTOM_COMMAND_CODE]
 
 class CustomTCPClient:
 
     async def run_client(self, addr, port, **kwargs):
         reader, writer = await asyncio.open_connection(addr, port)
-        uid = kwargs.get("uid", secrets.token_bytes(4))
+        uid = kwargs.get("uid", os.urandom(4))
         logging.info(f"Connected to {addr}:{port}")
-        message = SCORE_CODE + uid
+        message = random.choice(CODES) + uid + b'\x2d\x05\x03\x04\x05\x06\x07'
         writer.write(message)
         await writer.drain()
         writer.close()
