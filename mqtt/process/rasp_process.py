6336dc2da15595af79930da204fdf4d5bad7818d
giba <260553@studenti.unimore.it>
2022-12-04 16:02:48 +0100

    removed mcu testing object

--- mqtt/process/rasp_process.py
+++ mqtt/process/rasp_process.py
@@ -1,21 +1,22 @@
+### USE THIS TO RUN BRIDGE OBJECT ###
+
 import logging
 import asyncio
-
 
 from mqtt.device.bridge_object import BridgeObject
 from mqtt.resource.photodiode_sensor_resource import PhotoDiodeSensorResource
 
+
 async def main():
     try:
         bridge = BridgeObject()
-        mcu = PhotoDiodeSensorResource()
 
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        asyncio.get_event_loop().create_task(mcu.start_periodic_update())
         await bridge.start()
     except KeyboardInterrupt:
         exit()
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
