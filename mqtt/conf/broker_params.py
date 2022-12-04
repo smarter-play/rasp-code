6c5721b3ce38bce3a879346f8ef467ac7135ed64
giba <260553@studenti.unimore.it>
2022-12-04 16:03:34 +0100

    temporary broker switch

--- mqtt/conf/broker_params.py
+++ mqtt/conf/broker_params.py
@@ -1,8 +1,9 @@
 class CloudBrokerParams:
-    BROKER_ADDRESS = "smarterplayer.carminezacc.com"
+    #BROKER_ADDRESS = "smarterplayer.carminezacc.com"
+    BROKER_ADDRESS = "broker.hivemq.com"
     BROKER_PORT = 1883
     #BROKER_USERNAME = "smarterplayer"
     #BROKER_PASSWORD = "pswd"
-    BASE_TOPIC = "smarterplayer/"
-    BASKET_TOPIC = "basket/"
-    COURT_TOPIC = "court/"
+    BASE_TOPIC = "smarterplay"
+    BASKET_TOPIC = "basket"
+    COURT_TOPIC = "court"
