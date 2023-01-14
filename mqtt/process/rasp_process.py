1862eb4b902a87e01bf9c891c7d69d4450c69a67
Lorenzo Rutayisire <lorenzorutayisire@gmail.com>
2023-01-14 15:06:29 +0100

    Decouple Simulator from Server

--- mqtt/process/rasp_process.py
+++ mqtt/process/rasp_process.py
@@ -5,7 +5,6 @@ import asyncio
 import sys
 
 from mqtt.device.bridge_object import BridgeObject
-from mqtt.resource.mcu import MCU
 
 if sys.platform == 'win32':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
@@ -13,12 +12,10 @@ if sys.platform == 'win32':
 async def main():
     try:
         bridge = BridgeObject()
-        #mcu = MCU()
 
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         await bridge.start()
-        #loop.create_task(mcu.start_periodic_update())
     except KeyboardInterrupt:
         exit()
 
